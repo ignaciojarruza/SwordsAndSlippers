dad694db7d768bc08d0c211d1416acefd6c2ae27
ignaciojarruza <85847657+ignaciojarruza@users.noreply.github.com>
2023-03-06 12:20:44 -0500

    Updated battle interface with javadocs
    Documented the battle interface.

--- Arena/Battle.java
+++ Arena/Battle.java
@@ -1,5 +1,19 @@
 package Arena;
+
+/**
+ * Battle interface. A battle supports two player objects to battle until one is the victor.
+ * @author Ignacio Arruza
+ */
 public interface Battle {
+
+    /**
+     * Provides a summary of both players and their attributes.
+     * @return summary text
+     */
     public String provideSummary();
+
+    /**
+     * Begins battle between two players until one is the victor. Prints out who the winner is.
+     */
     public void battle();
 }
